0e433ee34d787c309d7b92ddd60fcf479081b000
oiradayo <76747755+oiradayo@users.noreply.github.com>
2021-01-09 22:52:27 +0900

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -1,6 +1,6 @@
 library("data.table")
 
-setwd("~/Desktop/datasciencecoursera/4_Exploratory_Data_Analysis/project/data")
+setwd("/Users/daisukeohnuki/ドキュメント/Johns Hopkins R/Johns Hopkins")
 
 #Reads in data from file then subsets data for specified dates
 powerDT <- data.table::fread(input = "household_power_consumption.txt"
